d4fa72e08366440669d3532bd36b4e685440fe9b
Rakesh <chipparakesh01@gmail.com>
2024-10-17 00:05:19 +0530

    modified the port number

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 // database
 require("./config/db");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const app = require("./app");
 
 //running the server
