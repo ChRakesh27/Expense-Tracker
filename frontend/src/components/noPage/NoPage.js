4d3f66d118e79bd7c05962cc277e4fcfb5b711bb
Rakesh <chipparakesh01@gmail.com>
2024-10-17 00:10:55 +0530

    added page Not found

---
+++ frontend/src/components/noPage/NoPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NoPage() {
+  return (
+    <div
+      style={{ width: "100%", height: "100Vh" }}
+      className="d-flex  justify-content-center align-items-center"
+    >
+      <h1>404 Page Not Found</h1>
+    </div>
+  );
+}
+
+export default NoPage;
