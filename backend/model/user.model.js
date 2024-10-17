4316d00f48b04b4293358c4ea93624ae030cbdf7
Rakesh <chipparakesh01@gmail.com>
2024-10-17 12:22:21 +0530

    added validation for password

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -19,6 +19,7 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: [true, "Password is required field!"],
+      minlength: 8,
       trim: true,
       select: false,
     },
--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -7,6 +7,7 @@ function Login() {
   // const API_URL = "http://localhost:3001/api/v1/";
   const navigate = useNavigate();
   const [isSignIn, setIsSignIn] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [userData, setUserData] = useState({
     userName: "",
     email: "",
@@ -15,79 +16,99 @@ function Login() {
 
   async function onSubmit(e) {
     e.preventDefault();
+    setIsLoading(true);
     let token;
-    if (isSignIn) {
-      token = await axios.get(
-        `${API_URL}/users/login?email=${userData.email}&password=${userData.password}`
-      );
-    } else {
-      token = await axios.post(`${API_URL}/users/register`, userData);
+    try {
+      if (isSignIn) {
+        token = await axios.get(
+          `${API_URL}/users/login?email=${userData.email}&password=${userData.password}`
+        );
+      } else {
+        token = await axios.post(`${API_URL}/users/register`, userData);
+      }
+      if (token) {
+        localStorage.setItem("authToken", JSON.stringify(token.data));
+        navigate("/");
+      }
+    } catch (e) {
+      console.log("error", e);
     }
-    if (token) {
-      localStorage.setItem("authToken", JSON.stringify(token.data));
-      navigate("/");
-    }
+    setIsLoading(false);
   }
 
   return (
-    <div className="login">
-      <div className="form-box">
-        <form className="form" onSubmit={onSubmit}>
-          <span className="title">{isSignIn ? "Sign In" : "Sign up"}</span>
-          <span className="subtitle">
-            {isSignIn
-              ? "Sign in to your account"
-              : "Create a free account with your email."}
-          </span>
-          <div className="form-container">
-            {!isSignIn && (
+    <>
+      {isLoading && (
+        <div className="loading">
+          <div className="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      )}
+      <div className="login">
+        <div className="form-box">
+          <form className="form" onSubmit={onSubmit}>
+            <span className="title">{isSignIn ? "Sign In" : "Sign up"}</span>
+            <span className="subtitle">
+              {isSignIn
+                ? "Sign in to your account"
+                : "Create a free account with your email."}
+            </span>
+            <div className="form-container">
+              {!isSignIn && (
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="User Name"
+                  required
+                  onChange={(e) => {
+                    setUserData((val) => ({
+                      ...val,
+                      userName: e.target.value,
+                    }));
+                  }}
+                />
+              )}
               <input
-                type="text"
+                type="email"
                 className="input"
-                placeholder="User Name"
+                placeholder="Email"
                 required
                 onChange={(e) => {
-                  setUserData((val) => ({ ...val, userName: e.target.value }));
+                  setUserData((val) => ({ ...val, email: e.target.value }));
                 }}
               />
-            )}
-            <input
-              type="email"
-              className="input"
-              placeholder="Email"
-              required
-              onChange={(e) => {
-                setUserData((val) => ({ ...val, email: e.target.value }));
-              }}
-            />
-            <input
-              type="password"
-              className="input"
-              placeholder="Password"
-              required
-              onChange={(e) => {
-                setUserData((val) => ({ ...val, password: e.target.value }));
-              }}
-            />
+              <input
+                type="password"
+                className="input"
+                placeholder="Password"
+                required
+                minLength={8}
+                onChange={(e) => {
+                  setUserData((val) => ({ ...val, password: e.target.value }));
+                }}
+              />
+              <i>Password should be min 8 characters</i>
+            </div>
+            <button className="btn btn-primary signUp">
+              {isSignIn ? "Sign In" : "Sign up"}
+            </button>
+          </form>
+          <div className="form-section">
+            <p>
+              {!isSignIn ? "Have an account?" : "Create an account?"}
+              <u
+                onClick={() => {
+                  setIsSignIn((val) => !val);
+                }}
+              >
+                {!isSignIn ? "Sign In" : "Sign up"}
+              </u>
+            </p>
           </div>
-          <button className="btn btn-primary signUp">
-            {isSignIn ? "Sign In" : "Sign up"}
-          </button>
-        </form>
-        <div className="form-section">
-          <p>
-            {!isSignIn ? "Have an account?" : "Create an account?"}
-            <u
-              onClick={() => {
-                setIsSignIn((val) => !val);
-              }}
-            >
-              {!isSignIn ? "Sign In" : "Sign up"}
-            </u>
-          </p>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
